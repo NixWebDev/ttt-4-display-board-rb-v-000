84dd71d984001311a078e874c433f44892e8f8f3
Nikki McNurlen <nikkirmcnurlen@gmail.com>
2018-09-07 00:13:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
 def display board(board)
   puts " #{board[0]} | #{board[1] | #{board[2]} "
   puts -----------
+  puts " #{board[3]} | #{board[4] | #{board[5]} "
+  
+  puts " #{board[0]} | #{board[1] | #{board[2]} "
 end  
